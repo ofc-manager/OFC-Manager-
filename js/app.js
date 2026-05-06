b4ea61374d086c31c416650cff4e9b9c95a0e0c5
ofc-manager <mhsweets2026@gmail.com>
2026-05-05 22:43:20 -0300

    feat: integrar tela de transferências ao app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import { HomeScreen } from '../screens/home.js';
 import { MarketScreen } from '../screens/market.js';
 import { MatchesScreen } from '../screens/matches.js';
+import { TransfersScreen } from '../screens/transfers.js';
 
 import { setupNavigation } from './navigation.js';
 
@@ -11,6 +12,7 @@ app.innerHTML = `
 ${HomeScreen()}
 ${MarketScreen()}
 ${MatchesScreen()}
+${TransfersScreen()}
 
 <div class="bottom-nav">
 
@@ -24,6 +26,10 @@ ${MatchesScreen()}
 
   <button class="nav-btn" data-screen="matchesScreen">
     Partidas
+  </button>
+
+  <button class="nav-btn" data-screen="transfersScreen">
+    Transferências
   </button>
 
 </div>
